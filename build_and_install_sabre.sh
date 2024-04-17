4b7d5f5debaf9f69ba356b4e3e2d0726a14196b8
Nikita Lazarev <niklz@csail.mit.edu>
2024-04-17 15:46:13 -0700

    firecracker updated

---
+++ build_and_install_sabre.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This scripts builds all components of Sabre.
+
+# Build Firecracker with Sabre:
+#   - main instructions: https://github.com/barabanshek/firecracker/tree/sabre/sabre .
+pushd firecracker/
+docker pull barabanshik/firecrackder_sabre_devctr:latest
+tools/devtool build --release --libc gnu
+
+FIRECRACKER_PATH=$(pwd)
+
+popd
+
+# Build firecracker-containerd with Sabre:
+#   - main instructions: https://github.com/barabanshek/firecracker-containerd/tree/sabre/sabre .
+pushd firecracker-containerd/sabre/
+./install_contrainerd.sh
+./configure_node_for_containerd.sh ${FIRECRACKER_PATH}
+./configure_node_for_containerd.sh ${FIRECRACKER_PATH} # need to run twice due to a bug
